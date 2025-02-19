539ec33f321a3375c6d44b3401837d7afa9d41bd
mjtahia.tm10 <mjtahiaa@gmail.com>
2025-02-19 17:52:50 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const copyDiscountCoupon = document.getElementById('discount-coupon');
+copyDiscountCoupon.addEventListener('click', function() {
+  const discountText = copyDiscountCoupon.innerText;
+  const textarea = document.createElement('textarea');
+  textarea.value = discountText;
+  document.body.appendChild(textarea);
+  
+  textarea.select();
+  textarea.setSelectionRange(0, 99999); 
+
+  document.execCommand('copy');
+
+  document.body.removeChild(textarea);
+  
+  copyDiscountCoupon.textContent = 'Copied!';
+  
+  setTimeout(() => {
+    copyDiscountCoupon.textContent = discountText;
+  }, 900);
+});
+let itemList = [] ;
+ function AddItem(item,price){
+    itemList.push(item);
+    renderList() ;
+ }
+ 
+ function renderList(){
+    let output = document.getElementById('cart-item')
+    output.innerHTML= '' ;
+    itemList.forEach((item,index)  =>{
+output.innerHTML += ` <div>
+${index + 1} . <sapn> ${item}</sapn>
+</div>`
+    })
+ }
